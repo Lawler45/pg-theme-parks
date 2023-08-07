67ac3d7b730b69a9f562e4385cc7f5d37d0544af
jimstev2447 <jim.stevenson2447@gmail.com>
2023-08-07 16:23:59 +0100

    add start of day 2 branch

--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,9 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+
+const { Pool } = require("pg")
+
+const connection = new Pool()
+
+module.exports = connection
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,7 @@
 const { parks, rides, stalls } = require("./data/index.js");
-
 const db = require("./connection");
+const format = require("pg-format");
+const { prepareRides } = require("./utils.js");
 
 function seed() {
   return db
@@ -19,12 +20,65 @@ function seed() {
     })
     .then(() => {
       return createParks();
+    })
+    .then(() => {
+      return createRides();
+    })
+    .then(() => {
+      return insertParks(parks);
+    })
+    .then(({ rows: parksData }) => {
+      return insertRides(rides, parksData);
     });
 }
 
 function createParks() {
   /* Create your parks table in the query below */
-  return db.query("");
+  return db.query(`
+    CREATE TABLE parks(
+      park_id SERIAL PRIMARY KEY,
+      park_name VARCHAR,
+      year_opened INT,
+      annual_attendance INT
+  )`);
+}
+function createRides() {
+  return db.query(`
+  CREATE TABLE rides(
+    ride_id SERIAL PRIMARY KEY,
+    ride_name VARCHAR,
+    year_opened INT,
+    votes INT,
+    park_id INT REFERENCES parks(park_id)
+)`);
 }
 
+function insertParks(parks) {
+  const formattedData = parks.map((park) => {
+    const { park_name, year_opened, annual_attendance } = park;
+    return [park_name, year_opened, annual_attendance];
+  });
+  const queryString = format(
+    `
+  INSERT INTO parks
+  (park_name, year_opened, annual_attendance)
+  VALUES %L RETURNING *;
+  `,
+    formattedData
+  );
+  return db.query(queryString);
+}
+
+function insertRides(rides, parks) {
+  const formattedRides = prepareRides(rides, parks);
+  const queryString = format(
+    `
+  INSERT INTO rides
+  (ride_name, year_opened, votes, park_id)
+  VALUES %L
+  `,
+    formattedRides
+  );
+  return db.query(queryString);
+}
 module.exports = seed;
